3429e38d66c8381f51d0818b184d645f92bb5403
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2010-09-16 16:13:38 +0000

    Simplified proof.

--- src/Prop/Fprop_div_sqrt_error.v
+++ src/Prop/Fprop_div_sqrt_error.v
@@ -43,15 +43,11 @@ rewrite <- Falign_spec_exp.
 destruct (Falign beta fx fy) as (p,t); destruct p; now apply Zeq_le.
 Qed.
 
-
 Theorem format_nx: forall x, format x -> exists fx:float beta, (x=F2R fx)%R /\ Fexp fx = cexp x.
 intros x; unfold generic_format.
 exists (Float beta (Ztrunc (scaled_mantissa beta (FLX_exp prec) x)) (cexp x)).
 split; auto.
 Qed.
-
-
-Variable Hp1 : Zlt 1 prec.
 
 Theorem div_error_FLX :
   forall Zrnd x y,
@@ -63,6 +59,11 @@ destruct (Req_dec y 0) as [Zy|Zy].
 now rewrite Zy, Rmult_0_r, Rminus_0_r.
 case (Req_dec (rounding beta (FLX_exp prec) Zrnd (x/y)) 0); intros Hr.
 rewrite Hr; ring_simplify (x-0*y)%R; assumption.
+assert (Zx: x <> R0).
+contradict Hr.
+rewrite Hr.
+unfold Rdiv.
+now rewrite Rmult_0_l, rounding_0.
 destruct (format_nx x Hx) as (fx,(Hx1,Hx2)).
 destruct (format_nx y Hy) as (fy,(Hy1,Hy2)).
 destruct (format_nx (rounding beta (FLX_exp prec) Zrnd (x / y))) as (fr,(Hr1,Hr2)).
@@ -71,55 +72,33 @@ now apply FLX_exp_correct.
 unfold Rminus; apply format_add with fx (Fopp beta (Fmult beta fr fy)); trivial.
 now rewrite Fopp_F2R,mult_F2R, <- Hr1, <- Hy1.
 (* *)
-apply Rle_lt_trans with (Rabs x).
-replace (x + - (rounding beta (FLX_exp prec) Zrnd (x / y) * y))%R with
-  (y * (-(rounding beta (FLX_exp prec) Zrnd (x / y) - x/y)))%R.
-2: field; assumption.
-rewrite Rabs_mult, Rabs_Ropp.
-apply Rmult_le_reg_l with (/Rabs y)%R.
-apply Rinv_0_lt_compat.
+destruct (relative_error_FLX_ex beta prec Hp Zrnd (x / y)%R) as (eps,(Heps1,Heps2)).
+apply Rmult_integral_contrapositive_currified.
+exact Zx.
+now apply Rinv_neq_0_compat.
+rewrite Heps2.
+rewrite <- Rabs_Ropp.
+replace (-(x + - (x / y * (1 + eps) * y)))%R with (x * eps)%R by now field.
+rewrite Rabs_mult.
+apply Rlt_le_trans with (Rabs x * 1)%R.
+apply Rmult_lt_compat_l.
 now apply Rabs_pos_lt.
-rewrite <- Rmult_assoc, Rinv_l.
-2: now apply Rabs_no_R0.
-rewrite Rmult_1_l, Hr1.
-rewrite <- Rabs_Rinv; trivial.
-rewrite <- Rabs_mult.
-replace (/y * x)%R with ((F2R fr -(F2R fr - x / y)))%R by (unfold Rdiv ; ring).
-apply Rle_trans with (2:=Rabs_triang_inv _ _).
-apply Rplus_le_reg_l with (Rabs (F2R fr - x / y)).
-apply Rle_trans with (2*Rabs (F2R fr - x / y))%R;[right; ring|idtac].
-ring_simplify (Rabs (F2R fr - x / y) + (Rabs (F2R fr) - Rabs (F2R fr - x / y)))%R.
-apply Rle_trans with (2* (ulp beta (FLX_exp prec) (F2R fr)))%R.
-apply Rmult_le_compat_l.
-auto with real.
-rewrite <- Hr1; left.
-apply ulp_error_f; trivial.
-now apply FLX_exp_correct.
-clear; intros; unfold FLX_exp; omega.
-unfold ulp.
-apply Rle_trans with (bpow 1*bpow (cexp (F2R fr)))%R.
-apply Rmult_le_compat_r.
-apply bpow_ge_0.
-simpl; unfold Zpower_pos; simpl; rewrite Zmult_1_r.
-replace 2%R with (Z2R 2) by easy.
-apply Z2R_le.
-clear; destruct beta; simpl.
-now apply Zle_bool_imp_le.
-rewrite <- bpow_add.
-unfold canonic_exponent, FLX_exp.
-destruct (ln_beta beta (F2R fr)).
-simpl projT1.
-rewrite <- Hr1 in a.
-specialize (a Hr); rewrite Hr1 in a.
-apply Rle_trans with (2:=proj1 a).
-apply ->bpow_le; omega.
-case (Req_dec x 0); intros Hxz.
-rewrite Hxz, Rabs_R0.
-apply bpow_gt_0.
-rewrite Hx2; unfold canonic_exponent, FLX_exp.
-ring_simplify (prec + (projT1 (ln_beta beta x) - prec))%Z.
-destruct (ln_beta beta x); simpl.
-now apply a.
+apply Rlt_le_trans with (1 := Heps1).
+change R1 with (bpow 0).
+apply -> bpow_le.
+clear -Hp. omega.
+rewrite Rmult_1_r.
+rewrite Hx2.
+unfold canonic_exponent.
+destruct (ln_beta beta x) as (ex, Hex).
+simpl.
+specialize (Hex Zx).
+apply Rlt_le.
+apply Rlt_le_trans with (1 := proj2 Hex).
+apply -> bpow_le.
+unfold FLX_exp.
+ring_simplify.
+apply Zle_refl.
 (* *)
 replace (Fexp (Fopp beta (Fmult beta fr fy))) with (Fexp fr + Fexp fy)%Z.
 2: unfold Fopp, Fmult; destruct fr; destruct fy; now simpl.
@@ -131,13 +110,14 @@ apply Rlt_le_trans with (Rabs y * bpow (Fexp fr))%R.
 apply Rmult_lt_compat_l.
 now apply Rabs_pos_lt.
 rewrite Rabs_Ropp.
-apply Rlt_le_trans with (ulp beta  (FLX_exp prec) (F2R fr)).
+replace (bpow (Fexp fr)) with (ulp beta (FLX_exp prec) (F2R fr)).
 rewrite <- Hr1.
-apply ulp_error_f; trivial.
+apply ulp_error_f.
 now apply FLX_exp_correct.
 clear; intros; unfold FLX_exp; omega.
-right; unfold ulp; apply f_equal.
-rewrite Hr2, <- Hr1; trivial. 
+exact Hr.
+unfold ulp; apply f_equal.
+now rewrite Hr2, <- Hr1.
 replace (prec+(Fexp fr+Fexp fy))%Z with ((prec+Fexp fy)+Fexp fr)%Z by ring.
 rewrite bpow_add.
 apply Rmult_le_compat_r.
@@ -147,6 +127,8 @@ ring_simplify (prec + (projT1 (ln_beta beta y) - prec))%Z.
 destruct (ln_beta beta y); simpl.
 left; now apply a.
 Qed.
+
+Variable Hp1 : Zlt 1 prec.
 
 Theorem sqrt_error_N :
   forall x, format x ->
