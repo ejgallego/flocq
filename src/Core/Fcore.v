ec58e33a355433b50cf5aab8195f02f717944771
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2010-10-01 12:42:02 +0000

    Added license banner to the .v files.

--- src/Core/Fcore.v
+++ src/Core/Fcore.v
@@ -1,3 +1,21 @@
+(*
+This file is part of the Flocq formalization of floating-point
+arithmetic in Coq: http://flocq.gforge.inria.fr/
+
+Copyright (C) 2010 Sylvie Boldo
+Copyright (C) 2010 Guillaume Melquiond
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+COPYING file for more details.
+*)
+
 Require Export Fcore_Raux.
 Require Export Fcore_defs.
 Require Export Fcore_float_prop.
